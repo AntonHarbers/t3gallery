c321ae6feb6e7c30462dab71a7527ef23387cb1f
Anton Harbers <Anton.Harbers23@gmail.com>
2024-06-29 12:16:44 +0200

    FEAT: added next Image tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = 'force-dynamic'
@@ -8,10 +9,10 @@ async function Images() {
   const images = await getMyImages()
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="w-48 flex flex-col">
-          <img src={image.url} />
+        <div key={image.id} className="w-48 h-48 flex flex-col">
+          <Image src={image.url} width={192} height={192} alt={image.name} />
           <h1>{image.name}</h1>
         </div>
       ))}
